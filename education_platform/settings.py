f162f650fb3295fd135b4e5489d92241fef5792c
Nuradil Zhasulan <nurikzhasulan96@gmail.com>
2024-04-22 13:14:14 +0500

    сделал миграцию с sqlite на postgre

--- education_platform/settings.py
+++ education_platform/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'education_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'Nuradil-105',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
